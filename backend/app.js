f8a67254e5f23db3ba98c68f49d345cca383e655
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2025-11-10 22:09:14 +0100

    reinitiate projet after following the openclassrooms course

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
+// app.js: Fichier principal de l'application Node.js
+
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
-const Book = require('./models/Books');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
+// Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Connexion à MongoDB réussie !'))
   .catch(err => console.log('❌ Connexion à MongoDB échouée !', err));
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Headers CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,37 +29,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/books', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body,
-    });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-      .catch(error => res.status(400).json({ error }));
-    });
+// Routes
+app.use('/api/book', bookRoutes); 
+app.use('/api/auth', userRoutes);
 
-app.get('/api/books', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-});
-
+// Route statique pour les images (middleware "static")
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
